6bb75989836c0d1451bd2b9116b13fc6dece2d33
Sylvain Fankhauser <sephi@fhtagn.top>
2022-12-07 08:15:02 +0100

    Fix secret key environment file

--- django.nix
+++ django.nix
@@ -71,7 +71,7 @@ let
           User = instanceName;
         };
         script = ''
-          test -s ${secretKeyFile} || ${dependencyEnv}/bin/python -c "from django.core.management.utils import get_random_secret_key; print(f'export SECRET_KEY=\"{get_random_secret_key()}\"')" > ${secretKeyFile} && exit 0
+          test -s ${secretKeyFile} || ${dependencyEnv}/bin/python -c "from django.core.management.utils import get_random_secret_key; print(f'SECRET_KEY=\"{get_random_secret_key()}\"')" > ${secretKeyFile} && exit 0
         '';
       };
 
